491a384aa8d8094c6895b32ecab53b4701649120
Guanciottaman <italia.matteo00@gmail.com>
2023-07-09 14:41:15 +0200

    Updated file structure

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='installreq',
-    version='0.0.1',
+    version='1.0.0',
     description='Installs all the requirements for a file and runs it',
     author='Guanciottaman',
     author_email='italia.matteo00@gmail.com',
